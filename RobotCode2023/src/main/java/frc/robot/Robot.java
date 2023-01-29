caf4c53a013971dea848067dd6095f64c39088db
team806 <118126987+team806@users.noreply.github.com>
2023-01-29 16:11:30 -0500

    Update Robot.java

--- RobotCode2023/src/main/java/frc/robot/Robot.java
+++ RobotCode2023/src/main/java/frc/robot/Robot.java
@@ -11,10 +11,10 @@ import com.ctre.phoenix.sensors.CANCoder;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -33,14 +33,7 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  // added definisions
-
   private final XboxController controller = new XboxController(0);
-
-  PIDController pid = new PIDController(0.02, 0, 0);
-  PIDController ballancePID = new PIDController(0.02, 0, 0);
-
-  PIDController ballancePid = new PIDController(0.02, 0, 0);
 
   public static final ADIS16470_IMU IMU = new ADIS16470_IMU();
 
@@ -62,6 +55,9 @@ public class Robot extends TimedRobot {
   CANCoder RR_coder = new CANCoder(10);
   CANCoder RL_coder = new CANCoder(12);
 
+  PIDController pid = new PIDController(0.02, 0, 0);
+  PIDController ballancePID = new PIDController(0.02, 0, 0);
+
   private double swerveRatio = -0.36;
   private double angSpeedMax = 0.3;
   private double magSpeedMax = 1 - (angSpeedMax * -swerveRatio);
@@ -69,11 +65,18 @@ public class Robot extends TimedRobot {
   double robotX = 0;
   double robotY = 0;
 
-  double gyroRoll;
-  double gyroPitch;
-  double gyroYaw;
-
   boolean onChargeStation;
+
+  double gyroYaw;
+  double CWgyroYaw;
+
+  double controllerMagMax = 0.8;
+  double controllerMagPow = 3;
+
+  double FR_coderPosition;
+  double FL_coderPosition;
+  double RR_coderPosition;
+  double RL_coderPosition;
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -100,10 +103,13 @@ public class Robot extends TimedRobot {
     RL_coder.configSensorDirection(true);
     RL_coder.configMagnetOffset(-28);
     IMU.calibrate();
+    IMU.setYawAxis(IMUAxis.kZ);
 
     SmartDashboard.putNumber("angSpeedMax", angSpeedMax);
     SmartDashboard.putNumber("magSpeedMax", magSpeedMax);
 
+    SmartDashboard.putNumber("controller Mag Max", controllerMagMax);
+    SmartDashboard.putNumber("controller Mag Power", controllerMagPow);
   }
 
   /**
@@ -119,48 +125,51 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    SmartDashboard.putNumber("FR codeer ang", FR_coder.getAbsolutePosition());
-    SmartDashboard.putNumber("FL codeer ang", FL_coder.getAbsolutePosition());
-    SmartDashboard.putNumber("RR codeer ang", RR_coder.getAbsolutePosition());
-    SmartDashboard.putNumber("RL codeer ang", RL_coder.getAbsolutePosition());
-
-    SmartDashboard.putNumber("FR ang motor", motor_FRang.get());
-    SmartDashboard.putNumber("FL ang motor", motor_FLang.get());
-    SmartDashboard.putNumber("RR ang motor", motor_RRang.get());
-    SmartDashboard.putNumber("RL ang motor", motor_RLang.get());
-
-    SmartDashboard.putNumber("FR mag motor", motor_FRmag.get());
-    SmartDashboard.putNumber("FL mag motor", motor_FLmag.get());
-    SmartDashboard.putNumber("RR mag motor", motor_RRmag.get());
-    SmartDashboard.putNumber("RL mag motor", motor_RLmag.get());
-
-    IMU.setYawAxis(IMUAxis.kX);
-    gyroRoll = IMU.getAngle();
-    IMU.setYawAxis(IMUAxis.kY);
-    gyroPitch = IMU.getAngle();
-    IMU.setYawAxis(IMUAxis.kZ);
+    // CCW = positive, centered to NORTH
     gyroYaw = IMU.getAngle();
-
-    double velocityX = ((wheelSpeed(motor_FRmag) * Math.cos(gyroYaw + FR_coder.getAbsolutePosition())) +
-        (wheelSpeed(motor_FLmag) * Math.cos(gyroYaw + FL_coder.getAbsolutePosition())) +
-        (wheelSpeed(motor_RRmag) * Math.cos(gyroYaw + RR_coder.getAbsolutePosition())) +
-        (wheelSpeed(motor_RLmag) * Math.cos(gyroYaw + RL_coder.getAbsolutePosition()))) / 4;
-    double velocityY = ((wheelSpeed(motor_FRmag) * Math.sin(gyroYaw + FR_coder.getAbsolutePosition())) +
-        (wheelSpeed(motor_FLmag) * Math.sin(gyroYaw + FL_coder.getAbsolutePosition())) +
-        (wheelSpeed(motor_RRmag) * Math.sin(gyroYaw + RR_coder.getAbsolutePosition())) +
-        (wheelSpeed(motor_RLmag) * Math.sin(gyroYaw + RL_coder.getAbsolutePosition()))) / 4;
-
-    robotX += velocityX;
-    robotY += velocityY;
-    SmartDashboard.putNumber("X", robotX);
-    SmartDashboard.putNumber("Y", robotY);
-
-  }
-
-  // notice, wheel speed is measured in Feet/second because its pronounced soccer
-
-  double wheelSpeed(WPI_TalonFX motor) {
-    return ((1 / 3) * Math.PI) * (((motor.getSelectedSensorPosition() / 2048) * 600) / 6.75);
+    // CW = positive, centered to EAST
+    CWgyroYaw = -(gyroYaw + 90);
+    // CW = positive, centered to EAST
+    FR_coderPosition = FR_coder.getAbsolutePosition();
+    FL_coderPosition = FL_coder.getAbsolutePosition();
+    RR_coderPosition = RR_coder.getAbsolutePosition();
+    RL_coderPosition = RL_coder.getAbsolutePosition();
+    /*
+     * double velocityX = ((wheelSpeed(motor_FRmag) * Math.cos(CWgyroYaw +
+     * FR_coderPosition)) +
+     * (wheelSpeed(motor_FLmag) * Math.cos(CWgyroYaw + FL_coderPosition)) +
+     * (wheelSpeed(motor_RRmag) * Math.cos(CWgyroYaw + RR_coderPosition)) +
+     * (wheelSpeed(motor_RLmag) * Math.cos(CWgyroYaw + RL_coderPosition))) / 4;
+     * double velocityY = ((wheelSpeed(motor_FRmag) * Math.sin(CWgyroYaw +
+     * FR_coderPosition)) +
+     * (wheelSpeed(motor_FLmag) * Math.sin(CWgyroYaw + FL_coderPosition)) +
+     * (wheelSpeed(motor_RRmag) * Math.sin(CWgyroYaw + RR_coderPosition)) +
+     * (wheelSpeed(motor_RLmag) * Math.sin(CWgyroYaw + RL_coderPosition))) / 4;
+     * 
+     * robotX += velocityX;
+     * robotY += velocityY;
+     * 
+     * SmartDashboard.putNumber("X velocity", velocityX);
+     * SmartDashboard.putNumber("Y velocity", velocityY);
+     * SmartDashboard.putNumber("X", robotX);
+     * SmartDashboard.putNumber("Y", robotY);
+     * SmartDashboard.putNumber("Yaw", gyroYaw);
+     */
+    /*
+     * SmartDashboard.putNumber("FR codeer ang", FR_coderPosition);
+     * SmartDashboard.putNumber("FL codeer ang", FL_coderPosition);
+     * SmartDashboard.putNumber("RR codeer ang", RR_coderPosition);
+     * SmartDashboard.putNumber("RL codeer ang", RL_coderPosition);
+     * 
+     * SmartDashboard.putNumber("FR ang motor", motor_FRang.get());
+     * SmartDashboard.putNumber("FL ang motor", motor_FLang.get());
+     * SmartDashboard.putNumber("RR ang motor", motor_RRang.get());
+     * SmartDashboard.putNumber("RL ang motor", motor_RLang.get());
+     * SmartDashboard.putNumber("FR mag motor", motor_FRmag.get());
+     * SmartDashboard.putNumber("FL mag motor", motor_FLmag.get());
+     * SmartDashboard.putNumber("RR mag motor", motor_RRmag.get());
+     * SmartDashboard.putNumber("RL mag motor", motor_RLmag.get());
+     */
   }
 
   /**
@@ -211,8 +220,10 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
+    controllerMagMax = SmartDashboard.getNumber("controller Mag Max", controllerMagMax);
+    controllerMagPow = SmartDashboard.getNumber("controller Mag Power", controllerMagPow);
 
-    drive(controller.getLeftX() * 0.6, controller.getLeftY() * 0.6, controller.getRightX());
+    drive(controller.getLeftX(), controller.getLeftY(), Math.pow(controller.getRightX(), controllerMagPow));
 
     if (controller.getAButtonPressed()) {
       IMU.reset();
@@ -221,18 +232,18 @@ public class Robot extends TimedRobot {
 
   private void drive(double x, double y, double z) {
 
-    double controlerAng = Math.atan2(y, x) - Math.toRadians(IMU.getAngle() - 90);// rotate by gyro for field
-    double controlermag = MathUtil.clamp(Math.hypot(x, y), -1, 1);
+    double controlerAng = Math.atan2(y, x) + Math.toRadians(gyroYaw + 90);// rotate by gyro for field
+    double controlermag = Math.pow(MathUtil.clamp(Math.hypot(x, y), -1, 1), controllerMagPow) * controllerMagMax;
     x = Math.cos(controlerAng) * controlermag;
     y = Math.sin(controlerAng) * controlermag;
 
     SmartDashboard.putNumber("final x", x);
     SmartDashboard.putNumber("final y", y);
 
-    moduleDrive(motor_FRang, motor_FRmag, FR_coder.getAbsolutePosition(), x + (z * -0.707106), y + (z * 0.707106));
-    moduleDrive(motor_FLang, motor_FLmag, FL_coder.getAbsolutePosition(), x + (z * 0.707106), y + (z * 0.707106));
-    moduleDrive(motor_RRang, motor_RRmag, RR_coder.getAbsolutePosition(), x + (z * -0.707106), y + (z * -0.707106));
-    moduleDrive(motor_RLang, motor_RLmag, RL_coder.getAbsolutePosition(), x + (z * 0.707106), y + (z * -0.707106));
+    moduleDrive(motor_FRang, motor_FRmag, FR_coderPosition, x + (z * -0.707106), y + (z * 0.707106));
+    moduleDrive(motor_FLang, motor_FLmag, FL_coderPosition, x + (z * 0.707106), y + (z * 0.707106));
+    moduleDrive(motor_RRang, motor_RRmag, RR_coderPosition, x + (z * -0.707106), y + (z * -0.707106));
+    moduleDrive(motor_RLang, motor_RLmag, RL_coderPosition, x + (z * 0.707106), y + (z * -0.707106));
   }
 
   void moduleDrive(WPI_TalonFX angMotor, WPI_TalonFX magMotor, double encoderAng, double x, double y) {
@@ -272,22 +283,58 @@ public class Robot extends TimedRobot {
   /** This function is called once when test mode is enabled. */
   @Override
   public void testInit() {
-    onChargeStation = false;
+    // onChargeStation = false;
+    robotX = 0;
+    robotY = 0;
   }
 
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {
     // test ballance code
+    // controller.setRumble(RumbleType.kLeftRumble, 1);
+    /*
+     * if (onChargeStation) {
+     * drive(0, ballancePID.calculate(gyroPitch, 0), 0);
+     * } else {
+     * drive(0, 0.10, 0);
+     * if (gyroPitch > 10) {
+     * onChargeStation = true;
+     * }
+     * }
+     */
 
-    if (onChargeStation) {
-      drive(0, ballancePID.calculate(gyroPitch, 0), 0);
-    } else {
-      drive(0, 0.1, 0);
-      if (gyroPitch > 10) {
-        onChargeStation = true;
-      }
+    drive(controller.getLeftX(), 0.0, 0.0);
+
+    if (controller.getAButtonPressed()) {
+      IMU.reset();
     }
+    double FRmoduleSpeed = moduleSpeed(motor_FRmag, motor_FRang);
+    double FRmoduleAng = CWgyroYaw + FR_coderPosition;
+    double velocityX = FRmoduleSpeed * Math.cos(FRmoduleAng);
+    double velocityY = FRmoduleSpeed * Math.sin(FRmoduleAng);
 
+    robotX += velocityX;
+    robotY += velocityY;
+    SmartDashboard.putNumber("X velocity FRmag", FRmoduleSpeed);
+
+    SmartDashboard.putNumber("X velocity", velocityX);
+    SmartDashboard.putNumber("Y velocity", velocityY);
+    SmartDashboard.putNumber("X", robotX);
+    SmartDashboard.putNumber("Y", robotY);
+    SmartDashboard.putNumber("Yaw", gyroYaw);
+  }
+
+  // notice, wheel speed is measured in Feet/20ms because its pronounced soccer
+  double moduleSpeed(WPI_TalonFX driveMotor, WPI_TalonFX steerMotor) {
+    return (driveMotor.getSelectedSensorVelocity() * Math.PI / 30720) - (swerveRatio * steerMotor.get());
+    /*
+     * velocity /2048 = r/100ms
+     * r/100ms /5 = r/cycle
+     * circumfernce = PI/3
+     * r/cycle *circumfernce = feet/cycle
+     * (velocity/10240)*(PI/3)
+     * (velocity*PI)/30720
+     */
   }
 }
